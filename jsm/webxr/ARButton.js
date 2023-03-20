6e47b418f1ba81a51c2ca08f89bf95e067341741
DbSundhar <111972678+DbSundhar@users.noreply.github.com>
2023-03-20 14:54:57 +0530

    Update ARButton.js

--- jsm/webxr/ARButton.js
+++ jsm/webxr/ARButton.js
@@ -12,7 +12,7 @@ class ARButton {
 				overlay.style.display = 'none';
 				document.body.appendChild( overlay );
 
-				const svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
+				/*const svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
 				svg.setAttribute( 'width', 38 );
 				svg.setAttribute( 'height', 38 );
 				svg.style.position = 'absolute';
@@ -23,13 +23,13 @@ class ARButton {
 					currentSession.end();
 
 				} );
-				overlay.appendChild( svg );
+				overlay.appendChild( svg );*/
 
 				const path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
 				path.setAttribute( 'd', 'M 12,12 L 28,28 M 28,12 12,28' );
 				path.setAttribute( 'stroke', '#fff' );
 				path.setAttribute( 'stroke-width', 2 );
-				svg.appendChild( path );
+				//svg.appendChild( path );
 
 				if ( sessionInit.optionalFeatures === undefined ) {
 
